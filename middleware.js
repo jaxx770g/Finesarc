9c6769cc3f21799a0162760e4e72c2c74e415f14
jaxx770g <jays86922@gmail.com>
2025-11-10 16:19:56 +0530

    middleware update

--- middleware.js
+++ middleware.js
@@ -1,15 +1,13 @@
-import {clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
+import {clerkMiddleware,createRouteMatcher}from'@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import { arcjet,createMiddleware,detectBot,shield} from"@arcjet/next";
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction(.*)",
 ]);const aj = arcjet({
   key: process.env.ARCJET_KEY,
-  
   rules: [
-    
     shield({
       mode: "LIVE",
     }),
@@ -18,30 +16,22 @@ const isProtectedRoute = createRouteMatcher([
       allow: [
         "CATEGORY:SEARCH_ENGINE", 
         "GO_HTTP", 
-        
       ],
     }),
   ],
 });
 const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
-
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
-
   return NextResponse.next();
 });
-
-
 export default createMiddleware(aj, clerk);
-
 export const config = {
   matcher: [
-   
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-
     '/(api|trpc)(.*)',
   ],
 };
